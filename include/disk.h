50501ca1a5278ca2760c9445cadca2bcd45e1320
bytorbix <bytorbix@gmail.com>
2025-11-03 15:55:02 +0200

    Implemented: fs_format, fs_mount, fs_unmount

--- include/disk.h
+++ include/disk.h
@@ -1,151 +1,151 @@
 
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdbool.h>
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <fcntl.h>
+    #include <stdlib.h>
+    #include <stdbool.h>
 
-// Size of each block
-#define BLOCK_SIZE 4096
+    // Size of each block
+    #define BLOCK_SIZE 4096
 
-// Structure of the disk
-typedef struct Disk Disk;
-struct Disk {
-    int     fd;         // File Descriptor (Proccess ID While Program is running)
-    size_t  blocks;     // Maximum Capacity of blocks in the disk
-    size_t  reads;      // Amount of reading operations (Used for benchmarking)
-    size_t  writes;     // Amount of writing operations (Used for benchmarking)
-    bool    mounted;    // Boolean representing if the disk is mounted or not
-};
+    // Structure of the disk
+    typedef struct Disk Disk;
+    struct Disk {
+        int     fd;         // File Descriptor (Proccess ID While Program is running)
+        size_t  blocks;     // Maximum Capacity of blocks in the disk
+        size_t  reads;      // Amount of reading operations (Used for benchmarking)
+        size_t  writes;     // Amount of writing operations (Used for benchmarking)
+        bool    mounted;    // Boolean representing if the disk is mounted or not
+    };
 
 
 
-/* Opens the file and returns the pointer to the virtual disk*/
-Disk * disk_open(const char *path, size_t blocks) {
+    /* Opens the file and returns the pointer to the virtual disk*/
+    Disk * disk_open(const char *path, size_t blocks) {
 
-    // Attempt to open the file and receive the proccess id (fd)
-    // if the file does not exist, a new one will be created.
-    int fd = open(path, O_RDWR | O_CREAT, 0666);
-    if (fd < 0) {
-        perror("disk_open: failed to open/create disk image");
-        return NULL;
-    }
-
-    // free up space and initiate the disk object and the pointer
-    Disk *disk = (Disk *)calloc(1, sizeof(Disk));
-    if (disk == NULL) {
-        close(fd); 
-        return NULL;
-    }
-
-    // calc size and resize if needed
-    off_t total_size = (off_t)blocks * BLOCK_SIZE;
-    if (ftruncate(fd, total_size) < 0) {
-        perror("disk_open: failed to resize disk image");
-        close(fd);
-        free(disk); 
-        return NULL;
-    }
-
-    // final construction 
-    disk->fd      = fd;
-    disk->blocks  = blocks;
-    disk->mounted = true;
-
-    return disk;
-}
-
-// closes the disk and frees up the memory
-void disk_close(Disk *disk) {
-    // error checks
-    if (disk == NULL) {
-        return;
-    }
-    if (disk->fd >= 0) {
-        if (close(disk->fd) < 0) {
-            perror("disk_close: Error closing the disk");
+        // Attempt to open the file and receive the proccess id (fd)
+        // if the file does not exist, a new one will be created.
+        int fd = open(path, O_RDWR | O_CREAT, 0666);
+        if (fd < 0) {
+            perror("disk_open: failed to open/create disk image");
+            return NULL;
         }
-    }
-    // unmount the disk and free it from the memory
-    disk->mounted = false;
-    disk->fd = -1; 
-    free(disk);
-}
 
-// write one block of data (4096 bytes) into the disk
-ssize_t disk_write(Disk *disk, size_t block, char *data) {
-    if (disk == NULL) {
-        perror("disk_write: disk is invalid");
-        return -1;
-    }
-    if (disk->mounted == false) {
-        perror("disk_write: disk is not mounted");
-        return -1;
-    }
-    if (block >= disk->blocks) {
-        perror("disk_write: block size is invalid");
-        return -1;
-    }
-
-    // position the offset in the file at desired block location
-    off_t offset = (off_t)block * BLOCK_SIZE;
-    if (lseek(disk->fd, offset, SEEK_SET) < 0) {
-        perror("disk_write: lseek has failed");
-        return -1;
-    }
-
-    // attempt to write the data into the disk
-    ssize_t written_bytes = write(disk->fd, data, BLOCK_SIZE);
-    if (written_bytes != BLOCK_SIZE) {
-        if (written_bytes < 0) {
-            perror("disk_write: write system call failed");
-        } else {
-            fprintf(stderr, "disk_write: short write (%zd bytes written instead of %d)\n", written_bytes, BLOCK_SIZE);
+        // free up space and initiate the disk object and the pointer
+        Disk *disk = (Disk *)calloc(1, sizeof(Disk));
+        if (disk == NULL) {
+            close(fd); 
+            return NULL;
         }
-        return -1;
-    }
-    // success
-    // write operation count is incremented
-    disk->writes++;
-    return written_bytes;
-}
 
-ssize_t disk_read(Disk *disk, size_t block, char *data) {
-    if (disk == NULL) {
-        perror("disk_read: disk is invalid (NULL pointer)");
-        return -1;
-    }
-    if (!disk->mounted) {
-        perror("disk_write: disk is not mounted");
-        return -1;
-    }
-    if (block >= disk->blocks) {
-        fprintf(stderr, "disk_read: block number %zu out of bounds (max %zu)\n", 
-                block, disk->blocks - 1);
-        return -1;
-    }
-    off_t offset = (off_t)block * BLOCK_SIZE;
-
-    if (lseek(disk->fd, offset, SEEK_SET) < 0) {
-        perror("disk_read: lseek has failed");
-        return -1;
-    }
-
-    // attempt to read the data from the disk into the buffer data
-    ssize_t bytes_read = read(disk->fd, data, BLOCK_SIZE);
-    if (bytes_read != BLOCK_SIZE) {
-        if (bytes_read < 0) {
-            perror("disk_read: system read call failed");
-        } else if (bytes_read == 0) {
-            fprintf(stderr, "disk_read: read failed, unexpectedly hit End-of-File (EOF)\n");
-        } else { 
-            fprintf(stderr, "disk_read: short read (%zd bytes read instead of %d)\n", 
-                    bytes_read, BLOCK_SIZE);
+        // calc size and resize if needed
+        off_t total_size = (off_t)blocks * BLOCK_SIZE;
+        if (ftruncate(fd, total_size) < 0) {
+            perror("disk_open: failed to resize disk image");
+            close(fd);
+            free(disk); 
+            return NULL;
         }
-        return -1; 
+
+        // final construction 
+        disk->fd      = fd;
+        disk->blocks  = blocks;
+        disk->mounted = true;
+
+        return disk;
     }
 
-    // increment the read operations and return the buffer
-    disk->reads++;
-    return bytes_read; 
-}
+    // closes the disk and frees up the memory
+    void disk_close(Disk *disk) {
+        // error checks
+        if (disk == NULL) {
+            return;
+        }
+        if (disk->fd >= 0) {
+            if (close(disk->fd) < 0) {
+                perror("disk_close: Error closing the disk");
+            }
+        }
+        // unmount the disk and free it from the memory
+        disk->mounted = false;
+        disk->fd = -1; 
+        free(disk);
+    }
+
+    // write one block of data (4096 bytes) into the disk
+    ssize_t disk_write(Disk *disk, size_t block, char *data) {
+        if (disk == NULL) {
+            perror("disk_write: disk is invalid");
+            return -1;
+        }
+        if (disk->mounted == false) {
+            perror("disk_write: disk is not mounted");
+            return -1;
+        }
+        if (block >= disk->blocks) {
+            perror("disk_write: block size is invalid");
+            return -1;
+        }
+
+        // position the offset in the file at desired block location
+        off_t offset = (off_t)block * BLOCK_SIZE;
+        if (lseek(disk->fd, offset, SEEK_SET) < 0) {
+            perror("disk_write: lseek has failed");
+            return -1;
+        }
+
+        // attempt to write the data into the disk
+        ssize_t written_bytes = write(disk->fd, data, BLOCK_SIZE);
+        if (written_bytes != BLOCK_SIZE) {
+            if (written_bytes < 0) {
+                perror("disk_write: write system call failed");
+            } else {
+                fprintf(stderr, "disk_write: short write (%zd bytes written instead of %d)\n", written_bytes, BLOCK_SIZE);
+            }
+            return -1;
+        }
+        // success
+        // write operation count is incremented
+        disk->writes++;
+        return written_bytes;
+    }
+
+    ssize_t disk_read(Disk *disk, size_t block, char *data) {
+        if (disk == NULL) {
+            perror("disk_read: disk is invalid (NULL pointer)");
+            return -1;
+        }
+        if (!disk->mounted) {
+            perror("disk_write: disk is not mounted");
+            return -1;
+        }
+        if (block >= disk->blocks) {
+            fprintf(stderr, "disk_read: block number %zu out of bounds (max %zu)\n", 
+                    block, disk->blocks - 1);
+            return -1;
+        }
+        off_t offset = (off_t)block * BLOCK_SIZE;
+
+        if (lseek(disk->fd, offset, SEEK_SET) < 0) {
+            perror("disk_read: lseek has failed");
+            return -1;
+        }
+
+        // attempt to read the data from the disk into the buffer data
+        ssize_t bytes_read = read(disk->fd, data, BLOCK_SIZE);
+        if (bytes_read != BLOCK_SIZE) {
+            if (bytes_read < 0) {
+                perror("disk_read: system read call failed");
+            } else if (bytes_read == 0) {
+                fprintf(stderr, "disk_read: read failed, unexpectedly hit End-of-File (EOF)\n");
+            } else { 
+                fprintf(stderr, "disk_read: short read (%zd bytes read instead of %d)\n", 
+                        bytes_read, BLOCK_SIZE);
+            }
+            return -1; 
+        }
+
+        // increment the read operations and return the buffer
+        disk->reads++;
+        return bytes_read; 
+    }
---
+++ include/fs.h
@@ -0,0 +1,272 @@
+/* File System */
+
+#include "disk.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+
+// File System Constants
+#define MAGIC_NUMBER (0xf0f03410)
+#define INODES_PER_BLOCK (128)
+#define POINTERS_PER_NODE (5)
+#define POINTERS_PER_BLOCK (1024)
+
+
+// File System Structure
+
+typedef struct SuperBlock SuperBlock;
+struct SuperBlock {
+    uint32_t magic_number;  // File system type identifier
+    uint32_t blocks;        // Total number of blocks in the file system
+    uint32_t inode_blocks;  // Total number of blocks reserved for the Inode Table
+    uint32_t inodes;        // Total number of Inode structures
+};
+
+
+typedef struct Inode Inode;
+struct Inode{
+    uint32_t valid;                      // Status: 1 if allocated (in use), 0 if free (available).
+    uint32_t size;                       // File size in bytes.
+    uint32_t direct[POINTERS_PER_NODE];  // Direct block addresses for the file's first data blocks.
+    uint32_t indirect;                   // Address of the single indirect block (1024 indirect pointers).
+};
+
+typedef union Block Block;
+union Block {
+    // Block Roles: A single block can only serve ONE of these purposes at a time.
+    
+    SuperBlock super;                      // File System Metadata: Contains the SuperBlock structure (Block 0).
+    Inode inodes[INODES_PER_BLOCK];        // Inode Table Block: Stores an array of 128 Inode structures (metadata for files).
+    uint32_t pointers[POINTERS_PER_BLOCK]; // Indirect Block: An array of block numbers used for large file addressing.
+    char data[BLOCK_SIZE];                 // Data Block: Raw storage for file content.
+};
+
+
+typedef struct FileSystem FileSystem;
+struct FileSystem {
+    Disk *disk;             // Instance of the emulated Disk
+    bool *free_blocks;      // Amount of available blocks
+    SuperBlock *meta_data;  // Meta data of the file system
+};
+
+/* File System Functions */
+
+void fs_debug(Disk *disk);
+
+
+bool fs_format(Disk *disk) 
+{
+    if (disk == NULL) {
+        perror("fs_format: Error disk is invalid");
+        return false;
+    }
+    if (disk->mounted) { 
+        fprintf(stderr, "fs_format: Error disk is mounted, aborting to prevent data loss\n");
+        return false;
+    }
+
+    // Initializing the super block
+    SuperBlock superblock;
+    superblock.magic_number = MAGIC_NUMBER;
+    superblock.blocks = (uint32_t)disk->blocks;
+
+    // Inodes
+    double percent_blocks = (double)superblock.blocks * 0.10;   
+    superblock.inode_blocks = (uint32_t)ceil(percent_blocks);
+    superblock.inodes = superblock.inode_blocks * INODES_PER_BLOCK;
+    
+    // Capacity check
+    if (1 + superblock.inode_blocks > superblock.blocks) {
+        fprintf(stderr, "fs_format: Error metadata blocks amount (%u) exceeds disk capacity (%u)\n", 
+                1 + superblock.inode_blocks, superblock.blocks);
+        return false;
+    }
+
+    // Initializing The Block Buffer
+    Block block_buffer;
+    for (int i = 0; i < BLOCK_SIZE; i++) {
+        block_buffer.data[i] = 0;
+    }
+
+    block_buffer.super = superblock; // Copy the initialized superblock into the union
+
+    // attempt to write the metadata into the magic block
+    if (disk_write(disk, 0, block_buffer.data) < 0) {
+        perror("fs_format: Failed to write SuperBlock to disk");
+        return false;
+    }
+
+    // Clean the inode tables
+    for (uint32_t i = 1; i <= superblock.inode_blocks; i++) {
+        if (disk_write(disk, i, block_buffer.data) < 0) {
+            perror("fs_format: Failed to clear inode table blocks");
+            return false;
+        }
+    }
+    // success
+    return true;
+}
+
+
+bool fs_mount(FileSystem *fs, Disk *disk) {
+    if (fs == NULL || disk == NULL) {
+        perror("fs_mount: Error fs or disk is invalid (NULL)"); 
+        return false;
+    }
+    if (disk->mounted) { 
+        fprintf(stderr, "fs_mount: Error disk is already mounted, cannot mount it\n");
+        return false;
+    }
+
+    Block block_buffer;
+    if (disk_read(disk, 0, block_buffer.data)< 0) {
+        perror("fs_mount: Failed to read super block from disk");
+        return false;
+    }
+
+    SuperBlock superblock = block_buffer.super;
+    if (superblock.magic_number != MAGIC_NUMBER) {
+        fprintf(stderr, 
+                "fs_mount: Error Disk magic number (0x%x) is invalid. Expected (0x%x).\n"
+                "The disk is either unformatted or corrupted.\n",
+                superblock.magic_number, MAGIC_NUMBER);
+        return false;
+    }
+    if (superblock.blocks != disk->blocks) {
+        fprintf(stderr, "fs_mount: Error Super block amount of blocks (%u) mismatch the disk capacity (%zu), aborting...\n",
+                superblock.blocks, disk->blocks);
+        return false;
+    }
+
+    // Allocate memory for the SuperBlock metadata and copy it
+    fs->meta_data = (SuperBlock *)malloc(sizeof(SuperBlock));
+    if (fs->meta_data == NULL) {
+        perror("fs_mount: Failed to allocate memory for SuperBlock metadata");
+        return false;
+    }
+
+    *(fs->meta_data) = superblock; // Copy the structure contents
+    fs->disk = disk;
+
+    uint32_t total_blocks = fs->meta_data->blocks;
+    fs->free_blocks = (bool *)calloc(total_blocks, sizeof(bool));
+
+    // Error check if free_blocks is invalid
+    if (fs->free_blocks == NULL) {
+        perror("fs_mount: Failed to allocate free block bitmap");
+        free(fs->meta_data);
+        return false;
+    }
+
+    // Set the super block (block 0) as allocated
+    fs->free_blocks[0] = true;
+
+    // Mark Inode blocks (Blocks 1 To N) as allocated
+    uint32_t inode_blocks_end = fs->meta_data->inode_blocks;
+    for (uint32_t i = 1; i <= inode_blocks_end; i++) {
+        // Check for bounds just in case
+        if (i < total_blocks) { 
+            // Mark as allocated
+            fs->free_blocks[i] = true;
+        }
+    }
+
+    // Scanning The Inode Tables
+    for (uint32_t i = 1; i <= inode_blocks_end; i++) {
+        Block inode_buffer;
+        // Attempt to copy the inode data on the disk into inode_buffer
+        if (disk_read(fs->disk, i, inode_buffer.data) < 0)
+        {
+            perror("fs_mount: failed to read Inode table");
+            free(fs->meta_data);
+            free(fs->free_blocks);
+            return false;
+        }
+        // Iterate through all the inodes inside the block
+        for (uint32_t j = 0; j < INODES_PER_BLOCK; j++)
+        {
+            // Check if inode is valid
+            if (inode_buffer.inodes[j].valid == true )
+                {
+                for (uint32_t k = 0; k < POINTERS_PER_NODE; k++)
+                {
+                    uint32_t block_num = inode_buffer.inodes[j].direct[k];
+                    // Check if pointer is non-zero AND within total disk bounds
+                    if (block_num != 0 && block_num < fs->meta_data->blocks) {
+                        fs->free_blocks[block_num] = true; // Mark as Allocated
+                    }
+                }
+                uint32_t indirect_block_num = inode_buffer.inodes[j].indirect;
+                uint32_t total_blocks = fs->meta_data->blocks;
+                if (indirect_block_num != 0 && indirect_block_num < total_blocks) {
+                    fs->free_blocks[indirect_block_num] = true;
+
+                    Block indirect_buffer;
+                    // Attempt to copy the indirect pointer into the indirect_buffer (basically the address)
+                    if (disk_read(fs->disk, indirect_block_num, indirect_buffer.data) < 0) {
+                        // Handle the read error and CLEAN UP ALL allocated memory
+                        perror("fs_mount: Failed to read indirect block during scan");
+                        free(fs->meta_data);
+                        free(fs->free_blocks);
+                        return false;
+                    }
+
+                    for (uint32_t k = 0; k < POINTERS_PER_BLOCK; k++) {
+                            uint32_t data_block_num = indirect_buffer.pointers[k];
+
+                            // Check if the pointer is non-zero AND within total disk bounds
+                            if (data_block_num != 0 && data_block_num < total_blocks) {
+                                fs->free_blocks[data_block_num] = true; // Mark the data block as allocated
+                            }
+                    }
+                }
+                
+            }
+        }
+        
+    }
+    disk->mounted=true;
+    return true;
+}
+
+
+void fs_unmount(FileSystem *fs) {
+    // Error Checks
+    if (fs == NULL) {
+        perror("fs_unmount: Error file system pointer is NULL");
+        return;
+    }
+    
+    // Check disk pointer 
+    if (fs->disk == NULL) {
+        perror("fs_unmount: Warning, disk pointer is NULL but continuing cleanup");
+    } else if (!fs->disk->mounted) { 
+        fprintf(stderr, "fs_unmount: Warning disk is already unmounted\n");
+        // Continue cleanup in case memory was still allocated
+    }
+
+    // --- Memory Cleanup ---
+    if (fs->meta_data != NULL) {
+        free(fs->meta_data);
+        fs->meta_data = NULL;
+    }
+    if (fs->free_blocks != NULL) {
+        free(fs->free_blocks);
+        fs->free_blocks = NULL;
+    }
+
+    if (fs->disk != NULL) {
+        // Dettach the disk from the file system
+        fs->disk->mounted = false;
+        fs->disk = NULL; // Clear the pointer to the disk
+    }
+}
+
+ssize_t fs_create(FileSystem *fs);
+bool fs_remove(FileSystem *fs, size_t inode_number);
+ssize_t fs_stat(FileSystem *fs, size_t inode_number);
+ssize_t fs_read(FileSystem *fs, size_t inode_number, char *data, size_t length, size_t offset);
+ssize_t fs_write(FileSystem *fs, size_t inode_number, char *data, size_t length, size_t offset) {
+
+}
